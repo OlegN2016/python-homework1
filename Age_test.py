05ca44bacfa896e663ff3e5b3eee4ffb2892adbc
OlegN2016 <oleg.n8@gmail.com>
2016-07-16 13:48:07 +0300

    Delete Age_test.py

--- Age_test.py
+++
@@ -1,9 +0,0 @@
-x = int(input ("Input your year of birth (in 4 digits):_____"))
-y = 2016 - x
-if y < 18:
-    print ("You're too YOUNG - TAKE A HIKE")
-elif y<60:
-    print ('Use moderation while consumming alchohol')
-else:
-    print ("You're an old ALCHOHOLIC! GO HOME!")
-    
